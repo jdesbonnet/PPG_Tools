9435aa61949529ebd72ed34548d0da43bf7e8fbf
Joe Desbonnet <jdesbonnet@gmail.com>
2017-07-26 23:07:52 +0100

    update CleanLevel1

--- scripts/CleanLevel1.java
+++ scripts/CleanLevel1.java
@@ -6,23 +6,51 @@ import java.io.*;
 public class CleanLevel1 {
 	public static void main (String[] arg) throws Exception {
 		String line;
-		long red,nir;
+		int red,nir,chk,hwt;
 
 		LineNumberReader r = new LineNumberReader(new InputStreamReader(System.in));
 		while ( (line = r.readLine()) != null) {
+
+			if (line.length() == 0) {
+				continue;
+			}
+
 			String[] p = line.split(" ");
 
+			if (p.length < 2) {
+				continue;
+			}
+
+			// All lines starting with # pass through
 			if (p[1].startsWith("#")) {
 				System.out.println(line);
 				continue;
 			}
 
+
+			// Remaining lines are formatted as <host-time-tag> $PPGV0 device red nir hwclock checksum
 			if (p.length != 7) {
-				System.err.println ("line " + r.getLineNumber() + ": wrong number of columns " + p.length);
+				System.err.println ("line " + r.getLineNumber() + ": wrong number of columns " + p.length + " line: " + line);
 				continue;
 			}
 			if ( ! p[1].startsWith("$PPG")  ) {
 				System.err.println ("line " + r.getLineNumber() + ": does not have $PPG header");
+				continue;
+			}
+
+			try {
+				//time = Double.parseDouble(p[0]);
+				red = Integer.parseInt(p[3],16);
+				nir = Integer.parseInt(p[4],16);
+				hwt = Integer.parseInt(p[5],16);
+				chk = Integer.parseInt(p[6].substring(1),16);
+                        } catch (NumberFormatException e) {
+                                System.err.println("line " + r.getLineNumber() + ": number parse exception " + e.getMessage());
+                                continue;
+                        }
+
+			if ( ((red+nir)&0xff) != chk) {
+				System.err.println("line " + r.getLineNumber() + ": checksum error ");
 				continue;
 			}
 
