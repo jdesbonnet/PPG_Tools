30faa9edf4075e1835706f65ca7deb4387f3c6d5
Joe Desbonnet <jdesbonnet@gmail.com>
2017-07-27 02:32:05 +0100

    no comment

---
+++ scripts/CleanLevel2.java
@@ -0,0 +1,63 @@
+import java.io.*;
+
+/**
+ * Remove records corrupted I2C bus errors etc. Assuming that
+ * Level1 clean has already been perfomed on data and has been
+ * processed by HighPass filter (providing raw, ac and dc 
+ * components of the signal).
+ */
+public class CleanLevel2 {
+	public static void main (String[] arg) throws Exception {
+		String line;
+		int sensor;
+		int red,nir,redac1,nirac1,redac2,nirac2,reddc,nirdc;
+		double time;
+		int hwclock;
+		int dropCount=0;
+		int totalRecords=0;
+
+		int threshold = 5000;
+
+		for (int i = 0; i < arg.length; i++) {
+			if ("-x".equals(arg[i])) {
+				threshold = Integer.parseInt(arg[i+1]);
+				i++;
+			}
+		}
+
+		LineNumberReader r = new LineNumberReader(new InputStreamReader(System.in));
+		while ( (line = r.readLine()) != null) {
+
+			totalRecords++;
+
+			String[] p = line.split(" ");
+
+			// Remaining lines are formatted as <host-time-tag> $PPGV0 device red nir hwclock checksum
+			if (p.length != 13) {
+				System.err.println ("line " + r.getLineNumber() + ": wrong number of columns " + p.length + " line: " + line);
+				continue;
+			}
+
+			time = Double.parseDouble(p[0]);
+			hwclock = Integer.parseInt(p[1]);
+			sensor = Integer.parseInt(p[2]);
+			red = Integer.parseInt(p[3]);
+			nir = Integer.parseInt(p[4]);
+			redac1 = Integer.parseInt(p[5]);
+			nirac1 = Integer.parseInt(p[6]);
+			redac2 = Integer.parseInt(p[7]);
+			redac2 = Integer.parseInt(p[8]);
+			reddc = Integer.parseInt(p[9]);
+			nirdc = Integer.parseInt(p[10]);
+
+			if ( Math.abs(redac2) > threshold) {
+				System.err.println ("line " + r.getLineNumber() + ": dropping record");
+				dropCount++;
+				continue;
+			}
+
+			System.out.println(line);
+		}
+		System.err.println ("dropped " + dropCount + " of " + totalRecords);
+	}
+}
--- scripts/runoff.sh
+++ scripts/runoff.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 SPS=100
 FFT_SIZE=4096
-gunzip -c ../data/20170723/overnight.ppg.gz | java FilterChannel 0 |  tail -n +100000 | java Clean | cut -d ' ' -f 1,6,7 | ./fft ${FFT_SIZE} ${SPS} > t.matrix
-gnuplot plot_waterfall.gnuplot
+#gunzip -c ../data/20170723/overnight.ppg.gz | java FilterChannel 0 |  tail -n +100000 | java Clean | cut -d ' ' -f 1,6,7 | ./fft ${FFT_SIZE} ${SPS} > t.matrix
+#gnuplot plot_waterfall.gnuplot
+
+
+gunzip -c ../../PPG_Data/data/20170725/ppg2.dat.gz | java CleanLevel1 | java FilterChannel 0 | java DeHex -t 1501040224 | java HighPassFilter | java CleanLevel2 -x 2000 > t.0
+gunzip -c ../../PPG_Data/data/20170725/ppg2.dat.gz | java CleanLevel1 | java FilterChannel 1 | java DeHex -t 1501040224 | java HighPassFilter | java CleanLevel2 -x 2000 > t.1
+
 
 
