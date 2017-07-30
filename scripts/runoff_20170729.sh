ab3decebfc0b5e04883d73fea462b1137d2957d7
Joe Desbonnet <jdesbonnet@gmail.com>
2017-07-30 17:11:20 +0100

    no comment

---
+++ scripts/runoff_20170729.sh
@@ -0,0 +1,12 @@
+gunzip -c ../../PPG_Data/data/20170729/ppg?.dat.gz | java CleanLevel1 | java FilterChannel 0 | java DeHex |java ZeroTime -t 1501298880 > t.0
+gunzip -c ../../PPG_Data/data/20170729/ppg?.dat.gz | java CleanLevel1 | java FilterChannel 1 | java DeHex |java ZeroTime -t 1501298880 > t.1
+
+octave ppg_filter.m t.0 filtered.0
+octave ppg_filter.m t.1 filtered.1
+
+java Pulse -x -5e-6 < filtered.0 > pulse.0
+java Pulse -x -5e-6 < filtered.1 > pulse.1
+java Phase pulse.0 pulse.1 > phase.dat
+
+
+
